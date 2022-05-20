39a74e1dcba1973d3d0d12ce548f0888e98bb236
Ivan Kedrov <104578896+ikedrov@users.noreply.github.com>
2022-05-20 17:04:47 +0300

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir main_folder
+cd main_folder
+mkdir folder7 folder8 folder9
+cd folder7
+touch 4.txt 5.txt 6.txt 3.json 4.json
+mkdir folder10 folder11 folder12
+ls -a
+mv 4.txt folder10 5.txt folder11
